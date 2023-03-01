415339ed83e5467943f70996f43765863e41ed7d
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-01 17:27:33 +0100

    errorHandling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,13 +5,16 @@ import { useEffect, useState } from "react";
 const AvailableMeals = () => {
   const [meals, setMealas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
   useEffect(() => {
     setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-df14a-default-rtdb.europe-west1.firebasedatabase.app/melas.json"
       );
-
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
       const loadedMeals = [];
 
@@ -26,13 +29,25 @@ const AvailableMeals = () => {
       setMealas(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
